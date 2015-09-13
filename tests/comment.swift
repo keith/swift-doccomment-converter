47646a062c5d191cb13760d298dbfb7ea97a24b7
Keith Smiley <keithbsmiley@gmail.com>
2015-09-12 20:12:02 -0700

    Add original test case

---
+++ tests/comment.swift
@@ -0,0 +1,20 @@
+    /**
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus risus ut laoreet posuere. Etiam
+    vestibulum elementum felis vitae sodales. Integer at varius dui. Maecenas et suscipit felis, sed cursus
+    ante. Aliquam erat volutpat. Aliquam vitae pellentesque dolor. Nunc in luctus tortor, non molestie nibh.
+
+    :param: method      Donec malesuada felis ligula, vitae cursus nibh consectetur nec
+    :param: parameters  Curabitur feugiat arcu magna, sit amet imperdiet est varius quis.
+    :param: options     Donec at consequat risus, in lacinia dui. Suspendisse potenti. Maecenas dignissim
+                        turpis in turpis hendrerit
+    :param: error       Fusce vulputate enim neque, vel mollis neque varius eu. Etiam quis quam sollicitudin,
+                        venenatis ex in, pretium enim. In aliquam diam id gravida fringilla.
+    :param: completion  Curabitur ac tempus sapien. Vivamus interdum ligula id nisi volutpat, et convallis
+    */
+    func something() -> Int {
+        if true {
+            return 1
+        }
+
+        return 0
+    }
